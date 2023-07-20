586ca147dc63d7dd01e2c130394f3d8d3b7982cb
alegarn <alexgarnier78310@protonmail.com>
2023-07-20 07:29:01 +0200

    Home/Hide screens, include navigation

---
+++ screens/HideScreen.js
@@ -0,0 +1,13 @@
+import { Pressable, StyleSheet, Text, View, Dimensions } from 'react-native';
+
+export default function HideScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hide Waldo</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
