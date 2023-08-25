e4b85fb0494c2b4373745fd7830de3d7fa66a14f
alegarn <alexgarnier78310@protonmail.com>
2023-08-25 12:43:11 +0200

    indentation

--- components/UI/TableButton.js
+++ components/UI/TableButton.js
@@ -4,11 +4,12 @@ import { GlobalStyle } from "../../constants/theme";
   export default function TableButton({cellData, rowData, onPress, windowHeight, windowWidth}) {
     return(
       <View style={[styles.btn, { width: windowWidth * 0.18, height: windowHeight * 0.03}]}>
-        <Pressable onPress={() => onPress(cellData, rowData)} style={ ({pressed}) => pressed && styles.pressed}>
+        <Pressable
+          onPress={() => onPress(cellData, rowData)}
+          style={ ({pressed}) => pressed && styles.pressed}>
             <Text style={styles.btnText}>More</Text>
         </Pressable>
       </View>
-
     );
   };
 
@@ -21,10 +22,8 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignSelf: "center",
   },
-
   btnText: {
     textAlign: 'center',
     color: GlobalStyle.color.tertiaryColor700,
   },
-
 });
