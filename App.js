53766aa7b07ace060ff8ef362d3f9bab275c80c1
alegarn <alexgarnier78310@protonmail.com>
2023-11-01 02:49:21 +0100

    simple interstitial ad (for android)

--- App.js
+++ App.js
@@ -28,6 +28,46 @@ import AuthContextProvider from './store/auth-context';
 import { AuthContext } from './store/auth-context';
 
 
+import 'expo-dev-client';
+
+
+import mobileAds, { MaxAdContentRating } from 'react-native-google-mobile-ads';
+
+import * as NavigationBar from "expo-navigation-bar";
+import { setStatusBarHidden } from "expo-status-bar";
+
+NavigationBar.setPositionAsync("relative");
+NavigationBar.setVisibilityAsync("hidden");
+NavigationBar.setBehaviorAsync("inset-swipe");
+setStatusBarHidden(true, "none");
+/*  */
+
+mobileAds()
+  .setRequestConfiguration({
+    // Update all future requests suitable for parental guidance
+    maxAdContentRating: MaxAdContentRating.PG,
+
+    // Indicates that you want your content treated as child-directed for purposes of COPPA.
+    tagForChildDirectedTreatment: true,
+
+    // Indicates that you want the ad request to be handled in a
+    // manner suitable for users under the age of consent.
+    tagForUnderAgeOfConsent: true,
+
+    // An array of test device IDs to allow.
+    testDeviceIdentifiers: ['EMULATOR'],
+  })
+  .then(() => {
+    console.log("Request config successfully set!");
+    mobileAds()
+    .initialize()
+    .then(adapterStatuses => {
+      console.log("Initialization complete!");
+    });
+  });
+
+
+
 
 const Stack = createNativeStackNavigator();
 
--- screens/AdScreen.js
+++ screens/AdScreen.js
@@ -1,5 +1,8 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState, useEffect } from "react";
 import { View, Text } from "react-native";
+
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { InterstitialAd, AdEventType, TestIds, useInterstitialAd } from 'react-native-google-mobile-ads';
 
 /* import { InterstitialAd, TestIds, AdEventType } from 'react-native-google-mobile-ads'; */
 /* const adUnitId = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy'; */
@@ -28,21 +31,20 @@ https://docs.page/invertase/react-native-google-mobile-ads/displaying-ads#inters
 /* European User Consent
 https://docs.page/invertase/react-native-google-mobile-ads/european-user-consent */
 
+
+
 export default function AdScreen({navigation, route}){
+
+
+  const showLoadingOverlay = () => {
+    const message = "Loading Ads";
+    return <LoadingOverlay message={message} />;
+  };
+
 
   const { onTarget, imageFile, pictureId, description, imageHeight, imageWidth, isPortrait, hiddenLocation, screenHeight, screenWidth, listId } = route.params;
 
-  /*
-  the options object includes the requestNonPersonalizedAdsOnly property, which tells the AdMob network to only show non-personalized ads
-  const interstitial = InterstitialAd.createForAdRequest(TestIds.INTERSTITIAL, {
-    requestNonPersonalizedAdsOnly: true,
-    keywords: ['fashion', 'clothing'],
-  }); */
-
-  const waitSomeTime = async (myFunction) => {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    myFunction();
-  };
+  /*  */
 
   const toResultScreen = () => {
     navigation.replace("ResultScreen", {
@@ -60,9 +62,48 @@ export default function AdScreen({navigation, route}){
     });
   };
 
-  useLayoutEffect(() => {
+
+  const { isLoaded, isClosed, load, show } = useInterstitialAd(TestIds.INTERSTITIAL, {
+    requestNonPersonalizedAdsOnly: true,
+  });
+
+  useEffect(() => {
+    // Start loading the interstitial straight away
+    load();
+  }, [load]);
+
+  useEffect(() => {
+    if (isClosed) {
+      // Action after the ad is closed
+      toResultScreen();
+    };
+  }, [isClosed, navigation]);
+
+
+  // No advert ready to show yet
+  if (!isLoaded) {
+    return showLoadingOverlay();
+  };
+
+
+
+  /*
+  the options object includes the requestNonPersonalizedAdsOnly property, which tells the AdMob network to only show non-personalized ads
+  const interstitial = InterstitialAd.createForAdRequest(TestIds.INTERSTITIAL, {
+    requestNonPersonalizedAdsOnly: true,
+    keywords: ['fashion', 'clothing'],
+  }); */
+
+/*   const waitSomeTime = async (myFunction) => {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    myFunction();
+  }; */
+
+
+
+/*   useLayoutEffect(() => {
     waitSomeTime(toResultScreen);
-  }, [route]);
+  }, [route]); */
 
 
   /*
@@ -76,7 +117,13 @@ export default function AdScreen({navigation, route}){
 
   return(
     <View>
-      <Text>AdScreen, do you want want to test every 5 sec with real ads ... ?</Text>
+      {isLoaded ? (
+          show()
+      )
+      : (
+        // No advert ready to show yet
+        showLoadingOverlay()
+      )}
     </View>
   )
 };
