0af0853bf3cf46997a65feca77c049a6a3b64bdc
alegarn <alexgarnier78310@protonmail.com>
2024-04-01 09:12:42 +0200

    comment console.log

--- screens/AuthScreens/LoginScreen.js
+++ screens/AuthScreens/LoginScreen.js
@@ -36,13 +36,13 @@ function LoginScreen() {
     setIsAuthenticating(true);
     try {
       const response = await login({email, password});
-      console.log("response login screen", response);
+      //console.log("response login screen", response);
       if (response.status === 200) {
         const auth = await handleAuthDataSaving(response, email);
         const scoreId = await handleScoreId();
         setIsAuthenticating(false);
       } else if (response.status === 401) {
-        console.log(response);
+        //console.log(response);
         Alert.alert('Invalid credentials, please retry', `Change your email or password before retrying \n ${response}`);
         setIsAuthenticating(false); 
       }
@@ -54,7 +54,7 @@ function LoginScreen() {
         setIsAuthenticating(false);
       };
     } catch (err) {
-      console.log(err);
+      //console.log(err);
       Alert.alert('There is an error', err);
       setIsAuthenticating(false);
     };
