f6c33fc7b0917b097818c534091d74f5b8b7f6c9
alegarn <alexgarnier78310@protonmail.com>
2024-06-19 03:59:08 +0200

    parameter for tutorial in HidingPath

--- screens/HideScreens/HidingPathScreen.js
+++ screens/HideScreens/HidingPathScreen.js
@@ -1,10 +1,10 @@
 import { View } from 'react-native';
 import LogicalImagePicker from '../../components/Picture/LogicalImagePicker';
 
-export default function HidingPathScreen({ navigation }) {
+export default function HidingPathScreen({ navigation, isTutorial }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <LogicalImagePicker navigation={navigation} />
+      <LogicalImagePicker navigation={navigation} isTutorial={isTutorial} />
     </View>
   );
 };
