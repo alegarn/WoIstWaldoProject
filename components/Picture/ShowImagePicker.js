8506148c41e031fe695146d3725f4b11461b92bf
alegarn <alexgarnier78310@protonmail.com>
2024-08-07 06:29:03 +0200

    a try using the Picture path for tutorial

--- components/Picture/ShowImagePicker.js
+++ components/Picture/ShowImagePicker.js
@@ -2,13 +2,17 @@ import { View, Image, StyleSheet } from 'react-native';
 
 import BigButton from '../UI/BigButton';
 import TutorialOverlay from '../UI/TutorialOverlay';
+import { useRef } from 'react';
 
 export default function ShowImagePicker({ takePictureHandler, image, imageWidth, imageHeight, pickImage }) {
-
+  const bigButtonRef = useRef(null);
   return (
     <View style={[styles.container, { paddingTop: image ? 10 : 0, justifyContent: image ? 'flex-start' : 'center' }]}>
       <View style={[styles.buttonsContainer, {marginTop: image ? 10 : 0,}]}>
-        <BigButton text="Take a Picture" onPress={takePictureHandler} />
+        <BigButton 
+          text="Take a Picture" 
+          onPress={takePictureHandler}
+          ref={bigButtonRef} />
         <BigButton text="Select an Image" onPress={pickImage} />
       </View>
       {image && (
@@ -19,8 +23,14 @@ export default function ShowImagePicker({ takePictureHandler, image, imageWidth,
       {isTutorial && (
         <TutorialOverlay 
           isVisible={true} 
-          targetPosition={bigButtonRef.current && bigButtonRef.current.measure()}
-          highlightArea={{height: 100, width: 100}} 
+          targetPosition={{
+            top: bigButtonRef.current.measure().pageY,
+            left: bigButtonRef.current.measure().pageX
+          }}
+          highlightArea={{
+            height: bigButtonRef.current && bigButtonRef.current.measure()?.height || 0, 
+            width: bigButtonRef.current && bigButtonRef.current.measure()?.width || 0
+          }} 
           instructions={"text"}
           instructionsPosition={{top: 50, left: 50}}
           onPress={() => {console.log("next")}} />
