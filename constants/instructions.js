75903796cb95da3023cc969e27a7fcdc1726c6e6
alegarn <alexgarnier78310@protonmail.com>
2024-08-29 08:58:24 +0200

    HomeScreen has TutorialOverlay

--- constants/instructions.js
+++ constants/instructions.js
@@ -1,5 +1,7 @@
 export const INSTRUCTIONS = {
   Tutorial: {
+    HomeScreen: "Welcome to WoIstWaldo tutorial. \n\n The game is simple. \n 1 - Choose a picture, or take it, which contains the thing (the Waldo) you want others to guess. Then point where you want to hide it. Finally describe it. \n\n 2 - Choose a picture from other players. Then read the description and guess what they wanted to hide. \n\n  Let's try it!",
+    HomeScreenModalBtn: "Hide the Waldo",
     HidingPathScreen: 
       "Hiding - Step 1 \n\n Take a picture with your phone that includes something you would like other to guess. (like a cat hidden in a tree or a special cloud in the sky) \n\n Don't make it too obvious for the others to guess to keep it fun to play. \n\n Example Image: Let's hide the horse in the image!",
     HidingPathScreenModalBtn: "Take a picture",  
