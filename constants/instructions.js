4592a5e17dcb3b6e2e07b8f604e193a19b88f0ab
alegarn <alexgarnier78310@protonmail.com>
2024-08-29 12:33:50 +0200

    HideScreen tutorial

--- constants/instructions.js
+++ constants/instructions.js
@@ -5,8 +5,8 @@ export const INSTRUCTIONS = {
     HidingPathScreen: 
       "Hiding - Step 1 \n\n Take a picture with your phone that includes something you would like other to guess. (like a cat hidden in a tree or a special cloud in the sky) \n\n Don't make it too obvious for the others to guess to keep it fun to play. \n\n Example Image: Let's hide the horse in the image!",
     HidingPathScreenModalBtn: "Take a picture", 
-    HidePathScreen: "",
-    HidePathScreenModalBtn: "", 
+    HideScreen: "Good, here is your picture!\nDid you decide what is the thing you want to hide ?\n\nYes ? Click on it when you see the image and then click again on the target when you think it's the correct area.\n\n Example: On the image i wanted to hide the goose, so i did click on it.",
+    HideScreenModalBtn: "Point the target", 
     GuessPathScreen: "",
   }
 };
