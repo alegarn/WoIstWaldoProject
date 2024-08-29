72bb5f38f95fe8e2e87da6e30d79a5a9778ec0bd
alegarn <alexgarnier78310@protonmail.com>
2024-08-29 09:47:08 +0200

    start HidePathScreen instructions

--- constants/instructions.js
+++ constants/instructions.js
@@ -4,7 +4,9 @@ export const INSTRUCTIONS = {
     HomeScreenModalBtn: "Hide the Waldo",
     HidingPathScreen: 
       "Hiding - Step 1 \n\n Take a picture with your phone that includes something you would like other to guess. (like a cat hidden in a tree or a special cloud in the sky) \n\n Don't make it too obvious for the others to guess to keep it fun to play. \n\n Example Image: Let's hide the horse in the image!",
-    HidingPathScreenModalBtn: "Take a picture",  
+    HidingPathScreenModalBtn: "Take a picture", 
+    HidePathScreen: "",
+    HidePathScreenModalBtn: "", 
     GuessPathScreen: "",
   }
 };
