b203ee2771a0921382ab045249d0b3e07feca0c9
alegarn <alexgarnier78310@protonmail.com>
2024-08-29 06:12:05 +0200

    instructions for the     HidingPathScreenModalBtn

--- constants/instructions.js
+++ constants/instructions.js
@@ -1,7 +1,8 @@
 export const INSTRUCTIONS = {
   Tutorial: {
     HidingPathScreen: 
-      "This is the first step, take a picture with your phone including something you would like other to guess. (like a cat hidden in a tree or a special cloud in the sky) \n\n Don't make it too obvious for the others to guess to keep it fun to play.",
+      "Hiding - Step 1 \n\n Take a picture with your phone that includes something you would like other to guess. (like a cat hidden in a tree or a special cloud in the sky) \n\n Don't make it too obvious for the others to guess to keep it fun to play. \n\n Example Image: Let's hide the horse in the image!",
+    HidingPathScreenModalBtn: "Take a picture",  
     GuessPathScreen: "",
   }
 };
