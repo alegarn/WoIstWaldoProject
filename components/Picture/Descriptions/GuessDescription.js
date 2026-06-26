68fbcc1e031f60a1a5ef2b76e75c930647f98741
alegarn <alexgarnier78310@protonmail.com>
2026-06-26 09:03:36 +0200

    fix: adjust bottom positioning of description area in GuessDescription component for improved layout

--- components/Picture/Descriptions/GuessDescription.js
+++ components/Picture/Descriptions/GuessDescription.js
@@ -21,7 +21,7 @@ export default function GuessDescription({ item, showFullDescription, toggleDesc
 const styles = StyleSheet.create({
   descriptionArea: {
     position: 'absolute',
-    bottom: '10%',
+    bottom: '5%',
     left: 0,
     right: 0,
     alignItems: 'center',
