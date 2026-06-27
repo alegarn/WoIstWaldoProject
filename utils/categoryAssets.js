8d35ada557ede810945143af85adc816d579ff07
alegarn <alexgarnier78310@protonmail.com>
2026-06-27 17:07:08 +0200

    feat: replace PNG images with WEBP format in category assets for improved performance

--- utils/categoryAssets.js
+++ utils/categoryAssets.js
@@ -1,14 +1,14 @@
 const CATEGORY_ASSETS = {
-  all: require('../assets/categories/all-image-cat.png'),
-  other: require('../assets/categories/other-image-cat.png'),
-  nature: require('../assets/categories/nature-image-cat.png'),
-  city: require('../assets/categories/city-image-cat.png'),
-  animals: require('../assets/categories/animals-image-cat.png'),
-  food: require('../assets/categories/food-image-cat.png'),
-  vehicles: require('../assets/categories/vehicles-image-cat.png'),
-  interiors: require('../assets/categories/interiors-image-cat.png'),
-  landmarks: require('../assets/categories/landmarks-image-cat.png'),
-  abstract: require('../assets/categories/abstract-image-cat.png'),
+  all: require('../assets/categories/all-image-cat.webp'),
+  other: require('../assets/categories/other-image-cat.webp'),
+  nature: require('../assets/categories/nature-image-cat.webp'),
+  city: require('../assets/categories/city-image-cat.webp'),
+  animals: require('../assets/categories/animals-image-cat.webp'),
+  food: require('../assets/categories/food-image-cat.webp'),
+  vehicles: require('../assets/categories/vehicles-image-cat.webp'),
+  interiors: require('../assets/categories/interiors-image-cat.webp'),
+  landmarks: require('../assets/categories/landmarks-image-cat.webp'),
+  abstract: require('../assets/categories/abstract-image-cat.webp'),
 };
 
 export function getCategoryAsset(key) {
